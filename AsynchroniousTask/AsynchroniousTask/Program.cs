42f2c9c0f711746eb6b83b0e197cdb47ec5ecd0d
SaiPranathiChitikeshi <75035288+SaiPranathiChitikeshi@users.noreply.github.com>
2021-01-02 18:53:31 +0530

    Add files via upload

---
+++ AsynchroniousTask/AsynchroniousTask/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asynchronious
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Task t = new Task(
+                () => {
+                    System.Threading.Thread.Sleep(5000);
+                    Console.WriteLine("Huge Task Finish");
+                }
+                );
+
+            //Start the Task  
+            t.Start();
+
+            //Wait for 1 second  
+            bool rValue = t.Wait(1000);
+            Console.WriteLine("Main Process Finished");
+            Console.ReadLine();
+            Task t1 = new Task(
+() => {
+    System.Threading.Thread.Sleep(5000);
+
+});
+            t1.Start();
+            Console.WriteLine("Cancelled:- " + t.IsCanceled);
+            Console.WriteLine("Completed:- " + t.IsCompleted);
+            Console.WriteLine("Folted:- " + t.IsFaulted);
+            Console.WriteLine("End of Main");
+            Console.ReadLine();
+
+
+
+            //parent and child class
+            Task Parent = new Task(
+() => {
+    Task Child = new Task(
+    () => {
+        System.Threading.Thread.Sleep(2000);
+        Console.WriteLine("Inner Task Finish");
+    },
+    TaskCreationOptions.AttachedToParent
+    );
+
+    //Start Child Task  
+    Child.Start();
+    System.Threading.Thread.Sleep(2000);
+    Console.WriteLine("Outer Task Finish");
+}
+);
+
+            //Start the Task  
+            Parent.Start();
+            Parent.Wait();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TaskBasedAsynchronousProgramming/TaskBasedAsynchronousProgramming/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+namespace TaskBasedAsynchronousProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using task 
+            Console.WriteLine($"Main Thread : {Thread.CurrentThread.ManagedThreadId} Statred");
+            Task task1 = new Task(PrintCounter);
+            task1.Start();
+            Console.WriteLine($"Main Thread : {Thread.CurrentThread.ManagedThreadId} Completed");
+            Console.ReadKey();
+
+            //using factory method
+            Console.WriteLine($"Main Thread : {Thread.CurrentThread.ManagedThreadId} Statred");
+            Task task2 = Task.Factory.StartNew(PrintCounter);
+            Console.WriteLine($"Main Thread : {Thread.CurrentThread.ManagedThreadId} Completed");
+            Console.ReadKey();
+
+            //using run method
+            Console.WriteLine($"Main Thread : {Thread.CurrentThread.ManagedThreadId} Statred");
+            Task task3 = Task.Run(() => { PrintCounter(); });
+            Console.WriteLine($"Main Thread : {Thread.CurrentThread.ManagedThreadId} Completed");
+            Console.ReadKey();
+        }
+        static void PrintCounter()
+        {
+            Console.WriteLine($"Child Thread : {Thread.CurrentThread.ManagedThreadId} Started");
+            for (int count = 1; count <= 5; count++)
+            {
+                Console.WriteLine($"count value: {count}");
+            }
+            Console.WriteLine($"Child Thread : {Thread.CurrentThread.ManagedThreadId} Completed");
+        }
+    }
+}
